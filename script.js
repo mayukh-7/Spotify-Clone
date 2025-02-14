3d0b64931a1375fcdc8979b0fe4837d3cbdb9176
mayukh-7 <mayukhmaity7@gmail.com>
2025-02-14 23:28:54 +0530

    Project Completed

--- script.js
+++ script.js
@@ -231,6 +231,10 @@ async function main() {
     document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
         console.log("Setting volume to", e.target.value, "/ 100");
         currentSong.volume = parseInt(e.target.value)/100;
+        if(currentSong.volume > 0)
+        {
+            document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("img/mute.svg" , "img/volume.svg");
+        }
     })
 
    // Add event listener to mute the volume
